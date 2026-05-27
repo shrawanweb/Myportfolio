560c9ad652ce199dd7b3c402c8eba5de96f6d7ff
Shrawan Kumar <shrawansaw89@gmail.com>
2026-05-27 16:17:29 +0530

    Update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1aLEERwmUhC-ipYUfdVBdNsW2qPDCukpE/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1AGmXen0Jv44KETUxcAsJ5anLJY32Kd-U/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
